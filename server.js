4581b3c36c95cb21b4ebc6d0a552fc24e625cc59
Sanock-anus <sashaonicum@gmail.com>
2024-12-28 17:39:44 +1000

    server.js

--- server.js
+++ server.js
@@ -1,26 +1,50 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Для разрешения запросов с разных доменов
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
-app.use(cors()); // разрешаем запросы с разных доменов
-app.use(bodyParser.json()); // используем bodyParser
+app.use(cors());
+app.use(bodyParser.json());
 
-let games = []; // храним данные об играх в массиве (вместо БД)
+// Путь к файлу, где будут храниться данные об играх
+const gamesFilePath = path.join(__dirname, 'games.json');
 
-// endpoint для загрузки игр
+// Функция для загрузки игр из файла
+function loadGames() {
+  try {
+    const data = fs.readFileSync(gamesFilePath, 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+      // Если файла нет или произошла ошибка при чтении, возвращаем пустой массив
+    return [];
+  }
+}
+
+// Функция для сохранения игр в файл
+function saveGames(games) {
+    fs.writeFileSync(gamesFilePath, JSON.stringify(games, null, 2));
+}
+
+
+// Endpoint для загрузки игр
 app.post('/upload', (req, res) => {
     const game = req.body;
+    const games = loadGames();
     games.push(game);
+    saveGames(games);
     res.json({ message: 'Игра загружена успешно!' });
 });
 
-// endpoint для получения публичных игр
+
+// Endpoint для получения публичных игр
 app.get('/games', (req, res) => {
-  const publicGames = games.filter(game => !game.private);
-  res.json(publicGames);
+    const games = loadGames();
+     const publicGames = games.filter(game => !game.private);
+     res.json(publicGames);
 });
 
 app.listen(port, () => {
